186bcf6188550cca51b4d005bd90a1b8034309d0
pamkirsten <69353348+pamkirsten@users.noreply.github.com>
2022-07-03 18:28:33 +0800

    generated backend and client

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useState, useEffect}  from 'react'
+
+
+function App() {
+  return (
+    <div>
+      <h1> Hello World</h1>
+
+    </div>
+    
+  );
+}
+
+export default App;
