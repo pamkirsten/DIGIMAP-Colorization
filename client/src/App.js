e8f7cc0ff614b5dd6491b91f10e8d0b255662306
camarillobryanb <bryan_daniel_camarillo@dlsu.edu.ph>
2022-07-03 22:30:11 +0800

    make it better

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,18 @@ import React, {useState, useEffect}  from 'react'
 
 
 function App() {
+
+  const [file, setFile] = useState();
+  function handleChange(e) {
+    console.log(e.target.files)
+    setFile(URL.createObjectURL(e.target.files[0]))
+  }
+
   return (
     <div>
-      <h1> Hello World</h1>
-      <h2> Test Edit </h2>
+      <h1>Colorization</h1>
+      <input type="file" id="image_input" accept="image/png, image/jpg" onChange={handleChange}></input>
+      <img src={file}/>
     </div>
     
   );
